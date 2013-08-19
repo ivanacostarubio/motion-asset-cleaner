2aee2bb80248b55596e9f3d5ed38d88a196cce41
Ivan Acosta-Rubio <ivan@bakedweb.net>
2013-08-19 09:48:45 -0500

    refactor to work :-)

--- spec/test_rm_asset_cleaner.sh
+++ spec/test_rm_asset_cleaner.sh
@@ -2,50 +2,49 @@ copy_image(){
     cp ./support/logo.png ./resources/$1
 }
 
-it_detects_the_files_in_a_project(){
-    assertEquals "./app/app_delegate.rb
-./app/onboarding_background_images.rb
-./lib/motion-asset-cleaner.rb" "`./find_files_in_project.sh`"
-}
-
 it_filters_out_default_images(){
     assertEquals "" "`./rm_asset_cleaner`"
 }
 
 it_knows_there_are_some_unused_assets(){
-    copy_image "unused_logo.png"
-    assertEquals "./resources/unused_logo.png" "`./rm_asset_cleaner`"
-    rm ./resources/unused_logo.png
+    copy_image "crazy.png"
+    copy_image "logo.png"
+    assertEquals "./resources/crazy.png" "`./rm_asset_cleaner`"
+    rm ./resources/crazy.png
+    rm ./resources/logo.png
 }
 
-it_knows_we_are_using_a_file_with_the_2x(){
+it_ignores_at_2x_when_present(){
     copy_image "unused_logo.png"
+    copy_image "logo.png"
     copy_image "logo@2x.png"
     assertEquals "./resources/unused_logo.png" "`./rm_asset_cleaner`"
     rm ./resources/logo@2x.png
     rm ./resources/unused_logo.png
+    rm ./resources/logo.png
 }
 
+it_detect_launch_images(){
+  copy_image "Default.png"
+  assertEquals "" "`./rm_asset_cleaner`"
+  rm ./resources/Default.png
+}
 
-it_detects_unused_at_2x_files(){
+#it_detects_unused_at_2x_files(){
+#    copy_image "not_used@2x.png"
+#    assertEquals "./resources/not_used@2x.png" "`./rm_asset_cleaner`"
+#    rm ./resources/not_used@2x.png
+#}
+
+#it_supports_interface_builder_files(){
     # TODO
-    echo
-}
+#    echo 
+#}
 
-it_detects_the_splash_image(){
+#it_supports_the_icons_files(){
     # TODO
-    echo 
-}
-
-it_supports_interface_builder_files(){
-    # TODO
-    echo 
-}
-
-it_supports_the_icons_files(){
-    # TODO
-    echo 
-}
+#    echo 
+#}
 
 suite(){
     for test_name in `grep '^it' $0 | cut -d '(' -f 1`; do 
